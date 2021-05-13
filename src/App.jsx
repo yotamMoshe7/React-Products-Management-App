d3d83b6040dee853876c835b24aab6b46ce94d1a
יותם <yotammoshe7@gmail.com>
2021-05-13 11:01:50 +0300

    Commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './App.css';
+import { TopBar } from './components/top-bar/TopBar';
+import { ButtonInput } from './components/buttons-input/ButtonInput';
+import ProductsList from './components/products-list/ProductsList';
+import { SELECT_LAST_ADDED } from './utility/Constants';
+
+function App() {
+  const [addButtonClick, setAddButtonClick] = useState(false);
+  const [searchField, setSearchField] = useState('');
+  const [selectSort, setSelectSort] = useState(SELECT_LAST_ADDED);
+
+  return (
+    <div className='app'>
+      <TopBar />
+      <ButtonInput
+        setSearchField={setSearchField}
+        setAddButtonClick={setAddButtonClick}
+        setSelectSort={setSelectSort}
+      />
+      <ProductsList
+        addButtonClick={addButtonClick}
+        setAddButtonClick={setAddButtonClick}
+        searchField={searchField}
+        selectSort={selectSort}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/form/Form.jsx
@@ -0,0 +1,214 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './Form.css';
+import { connect } from 'react-redux';
+import { initialProductArray } from '../../redux/items/items.actions';
+
+// Utilities
+import { inputsValidation, emptyProductObject } from '../../utility/Utility';
+
+// Functions for managing local storage
+import {
+  addProductToaLastAddedLocalStorage,
+  updateProductInLastAddedLocalStorage,
+  addProductToPriceSortedLocalStorage,
+  updateProductInPriceSortedLocalStorage,
+  addProductToTitleSortedLocalStorage,
+  updateProductInTitleSortedLocalStorage,
+  generateIdFromLocalStorage,
+  getListFromLocalStorage,
+} from '../../utility/LocalStorageManagement';
+
+// Componnets
+import { ProductImage } from '../product-image/ProductImage';
+import { CloseButton } from '../close-button/CloseButton';
+
+const DisplayElement = ({
+  initialProductArray,
+  addButtonClick,
+  setAddButtonClick,
+  currentItems,
+  markProductId,
+  setMarkProductId,
+  selectSort,
+}) => {
+  const [product, setProduct] = useState({
+    id: null,
+    imageUrl: null,
+    date: null,
+    title: '',
+    description: '',
+    price: '',
+  });
+  const [finishSaveProduct, setFinishSaveProduct] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const updateExistProduct = useRef(false);
+
+  // For distinguish between update to save new product
+  const saveNewProduct = useRef(false);
+
+  const handleChange = (event) => {
+    setProduct({ ...product, [event.target.name]: event.target.value });
+  };
+
+  // Add button clicked
+  useEffect(() => {
+    if (addButtonClick || finishSaveProduct) {
+      setProduct(emptyProductObject);
+      setMarkProductId(null);
+    }
+  }, [addButtonClick, finishSaveProduct, setMarkProductId]);
+
+  // Update form when product in products list has been clicked
+  useEffect(() => {
+    // Get product from list
+    let productToDisplay = currentItems.find(
+      (product) => product.id === markProductId
+    );
+    if (markProductId !== null) {
+      setProduct({
+        id: productToDisplay.id,
+        imageUrl: productToDisplay.imageUrl,
+        date: productToDisplay.date,
+        title: productToDisplay.title,
+        description: productToDisplay.description,
+        price: productToDisplay.price,
+      });
+    }
+  }, [markProductId, currentItems]);
+
+  // Update product
+  useEffect(() => {
+    setFinishSaveProduct(false);
+
+    // Check if need to update product
+    if (updateExistProduct.current) {
+      let validationResult = inputsValidation(product);
+      setErrorMessage(validationResult);
+
+      if (validationResult === null) {
+        // Update product to local storage lists
+        updateProductInLastAddedLocalStorage(product);
+        updateProductInPriceSortedLocalStorage(product);
+        updateProductInTitleSortedLocalStorage(product);
+
+        // Get relevant update products list elements and update state
+        let updateArray = getListFromLocalStorage(selectSort);
+        initialProductArray(updateArray);
+
+        setFinishSaveProduct(true);
+        updateExistProduct.current = false;
+      } else {
+        updateExistProduct.current = false;
+      }
+    }
+  }, [product, initialProductArray, selectSort]);
+
+  // Create Product
+  useEffect(() => {
+    setFinishSaveProduct(false);
+
+    // Check product date and id state is available
+    if (
+      product.date !== null &&
+      product.id !== null &&
+      saveNewProduct.current
+    ) {
+      let validationResult = inputsValidation(product);
+      setErrorMessage(validationResult);
+
+      if (validationResult === null) {
+        // Add product to local storage lists
+        addProductToaLastAddedLocalStorage(product);
+        addProductToPriceSortedLocalStorage(product);
+        addProductToTitleSortedLocalStorage(product);
+
+        // Get relevant update products list elements and update state
+        let updateArray = getListFromLocalStorage(selectSort);
+        initialProductArray(updateArray);
+
+        // addItem(product, selectSort);
+        setFinishSaveProduct(true);
+        saveNewProduct.current = false;
+      } else {
+        saveNewProduct.current = false;
+      }
+    }
+  }, [product, initialProductArray, selectSort]);
+
+  const saveButtonClicked = async () => {
+    // When product need to be update
+    if (markProductId !== null) {
+      updateExistProduct.current = true;
+      setProduct({ ...product, date: new Date() });
+    } else {
+      saveNewProduct.current = true;
+      setProduct({
+        ...product,
+        id: generateIdFromLocalStorage(),
+        date: new Date(),
+      });
+    }
+  };
+
+  return addButtonClick || markProductId !== null ? (
+    <div id='prducts-display-wrapper'>
+      <div id='prducts-display-sub-wrapper'>
+        <CloseButton
+          setAddButtonClick={setAddButtonClick}
+          setMarkProductId={setMarkProductId}
+          setProduct={setProduct}
+          emptyProductObject={emptyProductObject}
+        />
+
+        <div className='display-title'>Product Details</div>
+
+        <ProductImage product={product} setProduct={setProduct} />
+
+        <div className='display-title'>Title</div>
+        <input
+          id='product-name'
+          name='title'
+          type='text'
+          onChange={handleChange}
+          value={product.title}
+        />
+
+        <div className='display-title'>Description</div>
+        <textarea
+          id='product-description'
+          name='description'
+          value={product.description}
+          onChange={handleChange}
+        />
+
+        <div className='display-title'>Price</div>
+        <div id='bottom-wrapper'>
+          <input
+            id='product-price'
+            name='price'
+            type='number'
+            value={product.price}
+            onChange={handleChange}
+          />
+          <div id='error-wrapper'>
+            <div id='error-message'>{errorMessage}</div>
+          </div>
+          <button id='save-button' onClick={saveButtonClicked}>
+            Save
+          </button>
+        </div>
+      </div>
+    </div>
+  ) : null;
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  initialProductArray: (itemsArray) =>
+    dispatch(initialProductArray(itemsArray)),
+});
+
+const mapStateToProps = (state) => ({
+  currentItems: state.items.currentItems,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(DisplayElement);
